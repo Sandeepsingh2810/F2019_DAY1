70c2161bd6af1a2b75d3bb2ee4c1a0a4f9e48b83
macstudent <sandeepsingh2810>
2019-09-18 19:20:43 -0400

    [Sandeep] Comment added

--- src/com/Lambton/FirstDay.java
+++ src/com/Lambton/FirstDay.java
@@ -5,10 +5,18 @@ public class FirstDay
     public static void main(String[] args)
     {
      System.out.println("sandy");
+
+     //Variable declaration
     int a,b,c;
+
+    //VAlue Assignment
     a=10;
     b=20;
+
+    //perform add
     c=a+b;
+
+    //display Result
 
         System.out.println("SUM : "+ c);
 
