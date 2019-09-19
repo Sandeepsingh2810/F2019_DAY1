434bce2ac62727b8bb5fd6b2ff867ba0016493b7
macstudent <sandeepsingh2810>
2019-09-18 20:30:51 -0400

    [Sandeep] function added

--- src/com/Lambton/FirstDay.java
+++ src/com/Lambton/FirstDay.java
@@ -4,6 +4,9 @@ public class FirstDay
 {
     public static void main(String[] args)
     {
+
+    }
+    {
      System.out.println("sandy");
 
      //Variable declaration
@@ -17,8 +20,14 @@ public class FirstDay
 
     //display Result
 
-        System.out.println("SUM : "+ c);
+        System.out.println("SUM : " + c);
 
+
+        int x = Integer.MAX_VALUE;
+        System.out.println("X : " + x);
+
+        int y = x+1;
+        System.out.println("Y : " + y);
 
     }
 }
--- src/com/Lambton/Student.java
+++ src/com/Lambton/Student.java
@@ -1,10 +1,20 @@
 package com.Lambton;
 
-public class Student;
-
+public class Student
 {
+  int studentID;
+  String studentName;
 
- int studentID;
- String studentName;
+  void setValues()
+  {
+      studentID = 1;
+      studentName=  "Sandeep";
+  }
 
+  void display()
+  {
+      System.out.println("Student ID : " + studentID);
+      System.out.println("Student Name : " + studentName);
+
+  }
 }
